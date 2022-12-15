1f37aabe116dea9bb77026f9e4129eeddc38fcbd
Harris Abbasi <harrisabbasi01@hotmail.com>
2022-12-16 00:36:48 +0500

    Localisation of popup

--- public/partials/pick-pack-public-popup.php
+++ public/partials/pick-pack-public-popup.php
@@ -30,28 +30,27 @@ if(!$this->pick_pack_woo_in_cart($product_id) && ($cart_count !== $fragile_count
         <div class="popup-body">
             <!-- <div class="logo-container"> -->
                 <!-- <img class="pick-pack-logo" src="https://phpstack-851887-2938889.cloudwaysapps.com/wp-plugin-server/assets/Logo_Officiel_PickPack_2-removebg-preview.png" alt=""> -->
-            <h3>L'EMMALAGE REUTILISABLE, LA SOLUTION DU FUTUR.</h3>
+            <h3><?php esc_attr_e("Do you want a PickPack reusable package?","pick-pack") ?></h3>
 
-            <h4>Obtenez 10% de rabais sur votre prochaine commande!</h4>
+            <h4><?php esc_attr_e("As a thank you for using the PickPack service, the online store where you are shopping will offer you 10% off your next order*.","pick-pack") ?></h4>
             <!-- </div> -->
            
             <div class="popup_flex">
 
-                <img class="pick-pack-logo" src="https://phpstack-851887-2938889.cloudwaysapps.com/wp-plugin-server/assets/Logo_Officiel_PickPack_2-removebg-preview.png" alt="">
+                <img class="pick-pack-logo" src="https://phpstack-851887-2938889.cloudwaysapps.com/wp-plugin-server/assets/WhatsApp Image 2022-12-14 at 21.19.54.jpeg" alt="">
                 
 
                 <div class="popup_col">
                     <div class="cnt">
                         
-                        <p>
-                            Chez nous, nous pensons que le monde a besoin de meilleures solutions pour les emballages à usage unique. C'est pourquoi nous proposons désormais une alternative. Elle se nomme PickPack - un emballage de livraison réutilisable pour les commandes en ligne. Lorsque tu recevras ta commande, tu n'auras qu'à plier ton emballage au format lettre et le renvoyer dans une boite postale Poste Canada, tout simplement!
+                        <p><?php esc_attr_e("The PickPack option allows you to reduce your environmental footprint by returning your packaging after receiving your package.","pick-pack") ?>
                         </p>
-                        <!-- <p>
-                            PickPack est une solution très simple au problème toujours croissant des déchets d'emballage. PickPack réduit les déchets et permet d'économiser une grande quantité des émissions de CO2 par rapport aux emballages à usage unique. Ajoutez un PickPack à votre commande pour 3.99 $ et recevez vos marchandises dans un emballage réutilisable et écologique. Le retour est GRATUIT !
+                        <p><?php esc_attr_e("TRULY be part of the environmental solution for only $3.99.","pick-pack") ?>
                         </p>
-                        <p>
-                            Une solution à faible coût, mais à grand impact!
-                        </p> -->
+                        <p><?php esc_attr_e("The return is FREE and SIMPLE.","pick-pack") ?>
+                        </p>
+                        <p><?php esc_attr_e("See the return procedure (Collaboration with Canada Post)", "pick-pack") ?>
+                        </p>
                         
                     </div>
                     
@@ -59,15 +58,27 @@ if(!$this->pick_pack_woo_in_cart($product_id) && ($cart_count !== $fragile_count
 
                 <label class="form-control-checkbox">
                   <input type="checkbox" name="checkbox-pickpack"  id="checkbox-pickpack"/>
-                  Ma Commande sera livree au Canada
+                  <?php esc_attr_e("My order will be delivered in Canada", "pick-pack") ?>
                 </label>
 
                 <div class="popup-footer">
                     <div class="footer-flex">
-                        <button class=" pick_pack_button pick_pack_add" data-target="pick_pack_popup">Ajouter au panier</button>
+                        <button class=" pick_pack_button pick_pack_add" data-target="pick_pack_popup"><?php esc_attr_e("Yes, I want to make a difference.", "pick-pack") ?></button>
 
-                        <button type='button' class="toggle cancel-button">Non Merci</button>
+                        <button type='button' class="toggle cancel-button"><?php esc_attr_e("No thank you", "pick-pack") ?></button>
                     </div>
+                    <div class="centered">
+                        <p class="inline-text"><?php esc_attr_e("Initiative powered by PickPack", "pick-pack") ?> </p>
+                        <img class="inline-image" src="https://phpstack-851887-2938889.cloudwaysapps.com/wp-plugin-server/assets/Logo_Officiel_PickPack_2-removebg-preview.png" alt="">
+                    </div>
+                    
+                    <p class="conditions"><?php 
+                        $span_text = '<span>CONDITIONS : </span>';
+                        $string = esc_attr__("CONDITIONS : You must choose the PickPack option AND return your packaging as agreed.", "pick-pack");
+                        $string = str_replace("CONDITIONS : ", "",$string);
+                        echo $span_text . $string;
+                     ?>
+                    </p>
                     <!-- <h5>Prix : $3 <span><button data-target="pick_pack_popup" class="close toggle pick_pack_button pick_pack_add" data-target="pick_pack_popup">Ajouter au panier</button></span></h5> -->
 
                 </div>
